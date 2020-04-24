daf7a8f4636493fa0cbd467cd48ddf8e134a8363
Chinni Chaitanya <chchaitanya95@gmail.com>
2020-04-25 03:37:40 +0530

    :sparkles: Add setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", mode="r") as fd:
+    long_description = fd.read()
+
+setuptools.setup(
+    name="discord-logger-chinnichaitanya",
+    version="1.0.0",
+    author="Chaitanya Chinni",
+    description="Disccord Logger is a custom message logger to Discord for Python 3.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chinnichaitanya/python-discord-logger",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3",
+)
