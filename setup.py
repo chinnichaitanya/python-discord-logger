66bc77311bc57b3c3b70f7487f9a0487b2757f51
Chinni Chaitanya <chchaitanya95@gmail.com>
2020-06-19 21:24:12 +0530

    :bookmark: Version 1.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", mode="r") as fd:
 
 setuptools.setup(
     name="discord-logger",
-    version="1.1.1",
+    version="1.1.2",
     author="Chaitanya Chinni",
     description="Discord Logger is a custom message logger to Discord for Python 3+",
     license="MIT License",
