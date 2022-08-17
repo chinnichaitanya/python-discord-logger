c7ec20fcb38695d7d847cbc8759436fac20077a6
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-08-17 19:59:02 +0500

    add support for proxies

--- discord_logger/message_logger.py
+++ discord_logger/message_logger.py
@@ -59,8 +59,9 @@ class DiscordLogger:
         if self.default_level not in self.COLORS.keys():
             self.default_level = "default"
 
+        proxies = kwargs.get('proxies')
         self.discord = DiscordWebhook(
-            url=self.webhook_url, username=self.application_name
+            url=self.webhook_url, username=self.application_name, proxies=proxies
         )
 
     def __remove_embeds(self):
